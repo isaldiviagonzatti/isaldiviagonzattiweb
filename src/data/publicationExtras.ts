10dd9706cd05d697b9fe50120ab563f73b907b91
Ignacio Saldivia Gonzatti <ignaciosaldiviagonzatti@gmail.com>
2026-08-14 15:09:00 +0200

    small changes

--- src/data/publicationExtras.ts
+++ src/data/publicationExtras.ts
@@ -2,10 +2,10 @@ import citiesPdf from '../assets/publications/2022_Cities.pdf?url';
 import oceanPdf from '../assets/publications/2024_npjOS.pdf?url';
 
 interface PublicationExtra {
-  note?: string;
   links?: Array<{
     label: string;
     href: string;
+    icon?: 'osf';
   }>;
 }
 
@@ -13,11 +13,10 @@ export const publicationExtras: Record<string, PublicationExtra> = {
   '10.1038/s44183-024-00082-6': {
     links: [
       { label: 'PDF', href: oceanPdf },
-      { label: 'OSF', href: 'https://doi.org/10.17605/OSF.IO/7P3ED' }
+      { label: 'OSF', href: 'https://doi.org/10.17605/OSF.IO/7P3ED', icon: 'osf' }
     ]
   },
   '10.1016/j.cities.2022.104038': {
-    note: 'This publication originated from my BSc thesis',
     links: [{ label: 'PDF', href: citiesPdf }]
   }
 };
