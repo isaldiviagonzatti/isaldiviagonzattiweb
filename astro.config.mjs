d0247a10befddaec51678f091c3a3051d44ef270
Ignacio Saldivia Gonzatti <ignaciosaldiviagonzatti@gmail.com>
2026-08-14 16:31:35 +0200

    change landing page and access to subsections

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://www.ignaciosg.com',
   output: 'static',
+  redirects: {
+    '/publications': '/resume/#publications'
+  },
   build: {
     format: 'directory'
   }
